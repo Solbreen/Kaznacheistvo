742df92786355e0266465a55a2806b985e9712fc
Solbreen <rundinosawr@gmail.com>
2023-08-23 18:00:57 +0300

    Переделал kazfastmm

--- KAZNA/KazFastmm.ps1
+++ KAZNA/KazFastmm.ps1
@@ -12,17 +12,41 @@
     Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
 #>
 
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]$KSPath,
+
+    [Parameter()]
+    [int32]$JobLimit = 6,
+
+    [Parameter()]
+    [string]$TRSAPath,
+
+    # Parameter help description
+    [Parameter()]
+    [string]$Proxy
+)
+
 #region preferences
-$urlKS = "http://budget.gov.ru/epbs/registry/7710568760-KRKS/data?pageSize=20&pageNum=%page%&filteractual=true&filterstatus=ACTIVE&filter_start_accountnum=40102810&filter_not_stateks=KS3"
+$data = @()
 $urlBIK = "https://www.cbr.ru/s/newbik"
-$pagenum = 1
-$data = [System.Collections.ArrayList]::new()
-$proxy = @{
-    ProxyUseDefaultCredentials = $true
-    proxy                      = 'Http://172.23.17.3:8080'
+$SB = {
+    param($par, $pro)
+    $urlKS = "http://budget.gov.ru/epbs/registry/7710568760-KRKS/data?pageSize=20&pageNum=%page%&filteractual=true&filterstatus=ACTIVE&filter_start_accountnum=40102810&filter_not_stateks=KS3"
+    $data_json = @()
+    foreach ($jtem in $par) {
+        $requestparam = @{
+            'uri' = $urlKS.Replace("%page%", "$jtem")
+        }
+        if ($PSBoundParameters.ContainsKey('pro')) {
+            $requestparam.add('proxy', $Pro)
+            $requestparam.add('ProxyUseDefaultCredentials', $true) 
+        }
+        $data_json += Invoke-RestMethod @requestparam
+    }
+    $data_json
 }
-$KSPath = "c:\test\KSfast.csv"
-$TRSAPath = "c:\test\TRSAfast.csv"
 $tempkatalog = "$PSScriptroot\$(get-random -minimum 100000000 -maximum 100000000000000000)"
 $CurrentVP = $VerbosePreference
 $VerbosePreference = 'Continue'
@@ -49,99 +73,70 @@ foreach ($path in $KSPath, $TRSAPath) {
 }
 #endregion
 
-
-#region KS_Load
-#  Этот блок кода отвечает за загрузку справочника казначейских счетов
-do {
-    try {
-        $data_json = Invoke-RestMethod -Uri ($urlKS.Replace("%page%", "$pagenum")) -ErrorAction Stop @proxy -Verbose:$false
-    }
-    catch {
-        throw "$($_.Exception.Message)"
-    }
-    
-    if ( $pagenum -eq 1) {
-        $recordscounttotal = $data_json.recordcount
-        $pagecount = $data_json.pagecount
-    }
-    
-    foreach ($item in $data_json.data) {
-        [void]$data.add($item)
-    }
-    Write-Progress -Activity "Выгружаем данные с сайта" -Status "Страниц: $pagenum из $pagecount.   $([math]::round(($pagenum / $pagecount * 100), 1))%" -PercentComplete ($pagenum / $pagecount * 100)
-    $pagenum = $pagenum + 1
-} until ( $data.count -eq $recordscounttotal ) 
-Write-host "Загрузка данных осуществлена за время :"(get-date).Add(-$starttime).TimeOfDay.tostring() -ForegroundColor Green
-Write-host "recordscounttotal = " $recordscounttotal
-#endregion
-
-#region trsaformat
-#  Выделяем единые казначейские счета в отдельный справочник
-$keeptrsa = @( 'accountnum', 'tofkbik', 'opentofkcode' )
-$trsa = @()
-$trsa = ($data | Select-object -Unique -Property $keeptrsa)
-#endregion
-
-#region trsaformat with BIK
-# Создаём временный каталог
-try {
-    new-item -path "$tempkatalog" -ItemType Directory -ErrorAction Stop | Out-Null
+#region Запрос на кол-во страниц
+$firstpageparam = @(1)
+if ($PSBoundParameters.ContainsKey('proxy')) {
+    $firstpageparam += $Proxy
 }
-catch {
+try{
+    $firstpage = invoke-command $sb -ArgumentList $firstpageparam -ErrorAction Stop
+}
+catch{
     throw "$($_.Exception.Message)"
 }
+$data += $firstpage.data
+$pagecount = $firstpage.pagecount
+write-verbose "Общее число страниц для скачивания: $pagecount"
+#endregion
 
-#  Качаем zip файл с данным БИК, распаковываем из него xml документ с данными 
-$bikfile = "$tempkatalog\bik.zip"
-try {
-    Invoke-WebRequest -Uri $urlBIK -ErrorAction Stop @proxy -OutFile $bikfile 
-}
-catch {
-    throw "$($_.Exception.Message)"
-}
-Expand-Archive -path $bikfile  -DestinationPath $tempkatalog
+$PageRange = [math]::round(($pagecount * 4) / 100)
 
-# Получаем данные из xml файла, создаём пространство имен для работы с xml файлом
-Write-Verbose "Подгружаем данные из скачанного xml файла"
-[xml]$xml = get-content -path "$tempkatalog\*.xml"
-$ns = 'urn:cbr-ru:ed:v2.0'
-write-Verbose "Создаём объект с типом Xml.XmlNamespaceManager для добавления namespace '$ns'"
-$Namespace = New-Object -TypeName "Xml.XmlNamespaceManager" -ArgumentList $XML.NameTable
-$Namespace.AddNamespace('ed', $ns)
+$mod = $pagecount % $PageRange
+$numofit = ($pagecount - $mod) / $PageRange
+$Nmas = @(1..($numofit + 1))
+$stp = 1
 
-foreach ($item in $trsa) {
-    $bic = $item.tofkbik
-    $a = $XML.SelectNodes("//ed:BICDirectoryEntry[@BIC='$bic']", $Namespace) # Выбираем из xml файла часть кода c БИК территориального органа Федерального казначейства
-    $hasAccounts = $false 
-    foreach ($entry in $a) {
-        $NameCBR = ''
-        $el_info = $entry.selectsinglenode("ed:ParticipantInfo", $namespace) # Для удобства работы назначаем переменную огбъектом со свойствами participantinfo текущего БИК
-        $NameP = $el_info.namep
-        $tnp = $el_info.tnp
-        $nnp = $el_info.nnp
-        [array]$el_acc = $entry.SelectNodes("ed:Accounts", $Namespace) # Добавляем в массив все элементы свойства accounts из части кода текущего БИК
-        $hasAccounts = $el_acc.count -gt 0 # проверка на существование элементов в accounts. Если вызвать переменную, то вернется булево значение
-        if ($hasAccounts) {
-            $AccountCBRBIC = $el_acc[0].AccountCBRBIC
-            foreach ($i in $XML.SelectNodes("//ed:BICDirectoryEntry[@BIC='$AccountCBRBIC']", $Namespace)) {
-                # ищем cbrbik по xml файлу
-                $el_info = $i.selectsinglenode("ed:ParticipantInfo", $namespace)
-                $NameCBR = $el_info.namep
-            }
-        }
-    }
-    # Добавляем в элемент trsa свойство BankName и присваивается значение, зависящее от значения $hasAccounts
-    if ($hasAccounts) {
-        add-member -InputObject $item -Name "BankName" -MemberType NoteProperty -Value "$NameCBR//$NameP, $tnp $nnp"
+#region Создание массива с массивом чисел
+for ($i = 1; $i -le $numofit; ++$i) {
+    $enp = $i * $PageRange
+    if ($i -eq 1) {
+        $smas = @(2..($enp))    
     }
     else {
-        add-member -InputObject $item -Name "BankName" -MemberType NoteProperty -Value "$NameCBR, $tnp $nnp"
+        $smas = @($stp..($enp))   
     }
+    $Nmas[$i - 1] = $smas
+    $stp = 1 + $enp
 }
+$nmas[$numofit] = @($stp..$pagecount)
+write-verbose "Количество блоков страниц для скачивания: $($nmas.count)" 
 #endregion
 
-#region end
-#  Конец. Просто выгружаем файлы
+Write-Verbose "Очищаем место от старых задач"
+get-job | remove-job
+
+Write-Verbose "Запускаем задачи с лимитом одновременных выполнений: $JobLimit"
+foreach ($item in $Nmas) {
+    while ((get-job -state Running).count -ge $JobLimit) {
+        Write-Progress -Activity "Этап 1/2. Выгружаем данные с сайта" -Status "Блоков: $((get-job).count) из $($nmas.count).   $([math]::round(($((get-job).count) / $($nmas.count) * 100), 1))%" -PercentComplete ($((get-job).count) / $($nmas.count) * 100)
+        start-sleep -Seconds 1 
+    }
+    Write-Verbose "Запускаем задачу для обработки страниц с $($item[0]) по $($item[-1]) "
+    if ($PSBoundParameters.ContainsKey('proxy')) {
+        Start-Job -ScriptBlock $SB -ArgumentList $item, $proxy | Out-Null
+    }
+    else {
+        Start-Job -ScriptBlock $SB -ArgumentList (, $item) | Out-Null
+    }
+}    
+
+while((get-job -state Completed).count -ne $nmas.count){
+    Write-Progress -Activity "Этап 2/2. Ожидаем завершения работ" -Status "Завершено: $((get-job -state Completed).count) из $($nmas.count).   $([math]::round(($((get-job -state Completed).count) / $($nmas.count) * 100), 1))%" -PercentComplete ($((get-job -state Completed).count) / $($nmas.count) * 100)
+} 
+write-verbose "Получаем результаты задач"
+$data += (receive-job -job (get-job) -Keep).data
+
+Write-host "Загрузка данных осуществлена за время :"(get-date).Add(-$starttime).TimeOfDay.tostring() -ForegroundColor Green
 try {
     $data | select-object -property ksnumber, 
     @{ label = "clientname"; expr = { $_.clientname.replace("`n", "") } }, 
@@ -152,16 +147,96 @@ try {
     signls | Export-Csv -Path $KSPath -Encoding utf8 -NoTypeInformation -ErrorAction stop
 }
 catch {
-    throw "$($_.Exception.Message)"
+    throw "$($_.Exception.Message)" #под выражение или subexpression
 }
-try {
-    $trsa | Export-Csv -Path $TRSAPath -Encoding utf8 -NoTypeInformation -ErrorAction Stop
-}
-catch {
-    throw "$($_.Exception.Message)"
-}
-#endregion
 
-Remove-Item -Path $tempkatalog -Recurse
+if ($PSBoundParameters.ContainsKey('TRSAPath')) {
+    #region trsaformat
+    #  Выделяем единые казначейские счета в отдельный справочник
+    $keeptrsa = @( 'accountnum', 'tofkbik', 'opentofkcode' )
+    $trsa = @()
+    $trsa = ($data | Select-object -Unique -Property $keeptrsa)
+    #endregion
+
+    #region trsaformat with BIK
+    # Создаём временный каталог
+    try {
+        new-item -path "$tempkatalog" -ItemType Directory -ErrorAction Stop | Out-Null
+    }
+    catch {
+        throw "$($_.Exception.Message)"
+    }
+
+    $bikparam = @{
+        'uri' = $urlBIK
+    }
+    if ($PSBoundParameters.ContainsKey('proxy')) {
+        $bikparam.add('proxy', $Proxy)
+        $bikparam.add('ProxyUseDefaultCredentials', $true) 
+    }
+
+
+    #  Качаем zip файл с данным БИК, распаковываем из него xml документ с данными 
+    $bikfile = "$tempkatalog\bik.zip"
+    try {
+        Invoke-WebRequest @bikparam -OutFile $bikfile 
+    }
+    catch {
+        throw "$($_.Exception.Message)"
+    }
+    Expand-Archive -path $bikfile  -DestinationPath $tempkatalog
+
+    # Получаем данные из xml файла, создаём пространство имен для работы с xml файлом
+    Write-Verbose "Подгружаем данные из скачанного xml файла"
+    [xml]$xml = get-content -path "$tempkatalog\*.xml"
+    $ns = 'urn:cbr-ru:ed:v2.0'
+    write-Verbose "Создаём объект с типом Xml.XmlNamespaceManager для добавления namespace '$ns'"
+    $Namespace = New-Object -TypeName "Xml.XmlNamespaceManager" -ArgumentList $XML.NameTable
+    $Namespace.AddNamespace('ed', $ns)
+
+    foreach ($item in $trsa) {
+        $bic = $item.tofkbik
+        $a = $XML.SelectNodes("//ed:BICDirectoryEntry[@BIC='$bic']", $Namespace) # Выбираем из xml файла часть кода c БИК территориального органа Федерального казначейства
+        $hasAccounts = $false 
+        foreach ($entry in $a) {
+            $NameCBR = ''
+            $el_info = $entry.selectsinglenode("ed:ParticipantInfo", $namespace) # Для удобства работы назначаем переменную огбъектом со свойствами participantinfo текущего БИК
+            $NameP = $el_info.namep
+            $tnp = $el_info.tnp
+            $nnp = $el_info.nnp
+            [array]$el_acc = $entry.SelectNodes("ed:Accounts", $Namespace) # Добавляем в массив все элементы свойства accounts из части кода текущего БИК
+            $hasAccounts = $el_acc.count -gt 0 # проверка на существование элементов в accounts. Если вызвать переменную, то вернется булево значение
+            if ($hasAccounts) {
+                $AccountCBRBIC = $el_acc[0].AccountCBRBIC
+                foreach ($i in $XML.SelectNodes("//ed:BICDirectoryEntry[@BIC='$AccountCBRBIC']", $Namespace)) {
+                    # ищем cbrbik по xml файлу
+                    $el_info = $i.selectsinglenode("ed:ParticipantInfo", $namespace)
+                    $NameCBR = $el_info.namep
+                }
+            }
+        }
+        # Добавляем в элемент trsa свойство BankName и присваивается значение, зависящее от значения $hasAccounts
+        if ($hasAccounts) {
+            add-member -InputObject $item -Name "BankName" -MemberType NoteProperty -Value "$NameCBR//$NameP, $tnp $nnp"
+        }
+        else {
+            add-member -InputObject $item -Name "BankName" -MemberType NoteProperty -Value "$NameCBR, $tnp $nnp"
+        }
+    }
+    #endregion
+
+    #region end
+    #  Конец. Просто выгружаем файлы
+    try {
+        $trsa | Export-Csv -Path $TRSAPath -Encoding utf8 -NoTypeInformation -ErrorAction Stop
+    }
+    catch {
+        throw "$($_.Exception.Message)"
+    }
+    #endregion
+    Remove-Item -Path $tempkatalog -Recurse
+}
+
+
 $VerbosePreference = $CurrentVP 
 Write-host "Закончено :"((get-date)) -ForegroundColor Magenta
